2307354cf389ec3d50bc6f9f6418a26803d0ec62
Jakub Waszkiewicz <->
2024-10-19 23:35:08 +0200

    add test endpoint for mutation

--- k8s-generic-pod-mutator/main.py
+++ k8s-generic-pod-mutator/main.py
@@ -1,5 +1,5 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 
 app = FastAPI()
@@ -8,3 +8,22 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.post("/mutate")
+def mutate(request: dict = Body(...)):
+    uid = request["request"]["uid"]
+    object = request["request"]["object"]
+
+    print("Received an object to mutate:", object)
+
+    return {
+        "apiVersion": "admission.k8s.io/v1",
+        "kind": "AdmissionReview",
+        "response": {
+            "uid": uid,
+            "allowed": True,
+            "patchType": "JSONPatch",
+            "patch": "",
+        }
+    }
